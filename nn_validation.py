4858f0197beb41c105ba669677ceaf5c3b63c156
lucasant10 <lucasant@gmail.com>
2018-08-21 22:24:49 +0200

    organizing file

--- nn_validation.py
+++ nn_validation.py
@@ -9,7 +9,6 @@ warnings.warn = warn
 import configparser
 import os
 import argparse
-import math
 import numpy as np
 from scipy import interp
 from political_classification import PoliticalClassification
@@ -20,11 +19,6 @@ from sklearn.model_selection import StratifiedKFold
 from utils import save_report_to_csv
 from bow_classifier import generate_roc_curve
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-from sklearn.manifold import TSNE
-import plotly.offline as py
-import plotly.graph_objs as go
-
-
 import matplotlib
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
@@ -32,71 +26,17 @@ import matplotlib.pyplot as plt
 H5_FILE = 'cnn_model.h5'
 NPY_FILE = 'cnn_model.npy'
 
-def plot_words(data, start, stop, step):
-    trace = go.Scatter(
-        x = data[start:stop:step,0], 
-        y = data[start:stop:step, 1],
-        mode = 'markers',
-        text= word_list[start:stop:step]
-    )
-    layout = dict(title= 't-SNE 1 vs t-SNE 2',
-                  yaxis = dict(title='t-SNE 2'),
-                  xaxis = dict(title='t-SNE 1'),
-                  hovermode= 'closest')
-    fig = dict(data = [trace], layout= layout)
-    py.image.save_as(fig, filename='a-simple-plot.png')
-
 def load_file():
-    tweets = list()
+    texts = list()
     xl = pd.ExcelFile("Dados Rotulados.xlsx")
     df = xl.parse("Sheet1")
 
-    tweets = [tw for tw in df.iloc[:,1]]
+    texts = [tx for tx in df.iloc[:,1]]
     y_true = [1 if i==u'política' else 0 for i in df.iloc[:,2]]
-    return tweets, y_true
+    return texts, y_true
 
-if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser(
-        description='Validation political CNN model')
-    parser.add_argument('-h5',  default=H5_FILE)
-    parser.add_argument('-npy', default=NPY_FILE)
 
-    args = parser.parse_args()
-    H5_FILE = args.h5
-    NPY_FILE = args.npy
-
-    cf = configparser.ConfigParser()
-    cf.read("file_path.properties")
-    path = dict(cf.items("file_path"))
-    dir_in = path['dir_in']
-
-    tweets, y_true = load_file()
-    tp = TextProcessor()
-
-    pc = PoliticalClassification(H5_FILE, NPY_FILE, 25)
-
-    pol = ''
-    n_pol = ''
-    y_pred = list()
-    tweets = tp.text_process(tweets, text_only=True)
-    for tw in tweets:
-        text = ' '.join(tw)
-        if pc.is_political(text):
-            pol += text + '\n'
-            y_pred.append(1)
-        else:
-            n_pol += text + '\n'
-            y_pred.append(0)
-
-    print(classification_report(y_true, y_pred))
-    p, r, f1, s = precision_recall_fscore_support(y_true, y_pred)
-    
-    #try:
-    #generate_roc_curve (pc.get_classifier(), X, y_true,  model_name='CNN')
-    #except Exception as e:
-     #   print (e)
-
+def generate_roc_curve (X, y_true):
     cv = StratifiedKFold(n_splits=10)
     tprs = []
     aucs = []
@@ -104,9 +44,9 @@ if __name__ == "__main__":
     
     i = 0
 
-    for train, test in cv.split(tweets, y_true):
+    for train, test in cv.split(X, y_true):
 
-        x_test = np.array([tweets[i] for i in test])
+        x_test = np.array([X[i] for i in test])
         y_test = np.array([y_true[i] for i in test])
 
         y_probas = list()
@@ -154,6 +94,47 @@ if __name__ == "__main__":
     plt.savefig("plots/roc_curve_CNN.png")
     plt.clf()
 
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(
+        description='Validation political CNN model')
+    parser.add_argument('-h5',  default=H5_FILE)
+    parser.add_argument('-npy', default=NPY_FILE)
+
+    args = parser.parse_args()
+    H5_FILE = args.h5
+    NPY_FILE = args.npy
+
+    cf = configparser.ConfigParser()
+    cf.read("file_path.properties")
+    path = dict(cf.items("file_path"))
+    dir_in = path['dir_in']
+
+    X, y_true = load_file()
+    tp = TextProcessor()
+
+    pc = PoliticalClassification(H5_FILE, NPY_FILE, 25)
+
+    pol = ''
+    n_pol = ''
+    y_pred = list()
+    X = tp.text_process(X, text_only=True)
+    for tx in X:
+        text = ' '.join(tx)
+        if pc.is_political(text):
+            pol += text + '\n'
+            y_pred.append(1)
+        else:
+            n_pol += text + '\n'
+            y_pred.append(0)
+
+    print(classification_report(y_true, y_pred))
+    p, r, f1, s = precision_recall_fscore_support(y_true, y_pred)
+    
+    mean_auc, std_auc = generate_roc_curve (X, y_true)
+
     save_report_to_csv ('validation_report.csv', [ 
         'CNN',
         H5_FILE,
@@ -163,22 +144,7 @@ if __name__ == "__main__":
         s,
         mean_auc,
         std_auc
-    ])
-
-    # Zoom in view of the upper left corner.
-    #plt.figure(2)
-    #plt.xlim(0, 0.2)
-    #plt.ylim(0.8, 1)
-    #plt.plot([0, 1], [0, 1], 'k--')
-    #plt.plot(frps, tprs, label='CNN (AUC = {:.3f})'.format(auc_keras))
-    #plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
-    #plt.xlabel('False positive rate')
-    #plt.ylabel('True positive rate')
-    #plt.title('ROC curve (zoomed in at top left)')
-    #plt.legend(loc='best')
-    #plt.savefig('plots/cnn_roc_curve_zoom.png', dpi=400)
-
-    
+    ])    
     
     
     # f =  open(dir_in + "CSCW/politics.txt", 'w')
